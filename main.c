50a09e087baba041889e1c83bf324199a1f9d245
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-09 20:40:13 -0300

    maior que

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 5;
+    int b = 4;
+
+    printf("O resultado e: %d\n", (a + b));
+    printf("O resultado e: %d\n", (a - b));
+    printf("O resultado e: %d\n", (a / b));
+    printf("O resultado e: %d\n", (a * b));
+
+
+    return 0;
+}
