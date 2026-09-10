e6420281b9c8dae1c07e293715886370b5e9da1e
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-09 21:34:41 -0300

    maior que

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<windows.h>
 
 int main(){
+
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
 
     int a = 5;
     int b = 4;
 
-    printf("O resultado e: %d\n", (a + b));
-    printf("O resultado e: %d\n", (a - b));
-    printf("O resultado e: %d\n", (a / b));
-    printf("O resultado e: %d\n", (a * b));
+    printf("O resultado é: %d\n", (a + b));
+    printf("O resultado é: %d\n", (a - b));
+    printf("O resultado é: %d\n", (a / b));
+    printf("O resultado é: %d\n", (a * b));
 
 
     return 0;
