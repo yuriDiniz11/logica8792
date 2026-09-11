42a5d44cefc34cdfdc9ccc5c5bcc87c158cb4e16
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-11 20:39:58 -0300

    Conversão entre real e dolar

--- main.c
+++ main.c
@@ -1,25 +1,19 @@
 #include<stdio.h>
 #include<windows.h>
-#include<stdbool.h>
 
-bool ehPar(int numero){
-    return(numero % 2 == 0);
-
-}
 
 int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   float valor, cotacao;
+   float raio;
+   const float PI = 3.14159;
 
-   printf("Digite o valor em reais: ");
-   scanf("%f", &valor);
-   printf("Digite a cotação do dolar: ");
-   scanf("%f", &cotacao);
-
-   printf("Valor em dolares: %.2f\n", valor / cotacao);
+   printf("Digite o raio do circulo: ");
+   scanf("%f", &raio);
+   printf("Area: %.2f\n", PI * raio * raio);
+   printf("Perimetro: %.2f\n", 2 * PI * raio);
     
     return 0;
 }
