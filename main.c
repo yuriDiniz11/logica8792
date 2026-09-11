9f7304dcb379337169a1421264b1023092154103
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-11 19:37:47 -0300

    maior que

--- main.c
+++ main.c
@@ -12,17 +12,13 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    bool resultado1 = ehPar(4);
-    bool resultado2 = ehPar(7);
-    bool resultado3 = ehPar(11);
-    bool resultado4 = ehPar(8);
-    bool resultado5 = ehPar(5);
+    int n;
 
-    printf("4 é par? %d\n", resultado1);
-    printf("7 é par? %d\n", resultado2);
-    printf("11 é par? %d\n", resultado3);
-    printf("8 é par? %d\n", resultado4);    
-    printf("5 é par? %d\n", resultado5);
+    printf("Digite um número: ");
+    scanf("%d", &n);
+
+    printf("Antecessor: %d\n", n - 1);
+    printf("Sucessor: %d\n", n + 1);
     
     return 0;
 }
