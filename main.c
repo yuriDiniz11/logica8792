90f78ea9202b37c8383cd2bf52a0394da62e39b2
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-11 21:18:29 -0300

    Media entre 3 notas

--- main.c
+++ main.c
@@ -7,13 +7,14 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   float raio;
-   const float PI = 3.14159;
+   float n1, n2, n3, media;
 
-   printf("Digite o raio do circulo: ");
-   scanf("%f", &raio);
-   printf("Area: %.2f\n", PI * raio * raio);
-   printf("Perimetro: %.2f\n", 2 * PI * raio);
-    
+   printf("Digite três notas: ");
+   scanf("%f %f %f", &n1, &n2, &n3);
+
+   media = (n1 + n2 + n3) / 3;
+
+   printf("Média: %.2f\n", media);
+
     return 0;
 }
