bcd22ff723a8068f438b8ffcfeb82f8461a994b9
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-11 21:49:11 -0300

    Uso de if e else

--- main.c
+++ main.c
@@ -7,16 +7,18 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int segundos, horas, minutos, resto;
+   int n;
 
-   printf("Digite a quantidade de segundos: ");
-   scanf("%d", &segundos);
+   printf("Digite um número: ");
+   scanf("%d", &n);
 
-   horas = segundos / 3600;
-   minutos = (segundos % 3600) / 60;
-   resto = segundos % 60;
 
-   printf("%d horas, %d minutos e %d segundos\n", horas, minutos, resto);
+
+   if(n > 0){
+        printf("Positivo\n");
+}else{
+        printf("Negativo\n");
+}
 
     return 0;
 }
