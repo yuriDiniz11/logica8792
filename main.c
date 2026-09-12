0ed206655e086ddf53e6000939e0c41fb519ea0e
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-11 21:28:10 -0300

    Calcius para fahrenheit

--- main.c
+++ main.c
@@ -7,14 +7,13 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   float n1, n2, n3, media;
+   float celsius, fahrenheit;
 
-   printf("Digite três notas: ");
-   scanf("%f %f %f", &n1, &n2, &n3);
+   printf("Digite a temperatura em Celsius: ");
+   scanf("%f", &celsius);
 
-   media = (n1 + n2 + n3) / 3;
-
-   printf("Média: %.2f\n", media);
+   fahrenheit = (celsius * 9.0 / 5.0) + 32;
+   printf("Fahrenheit: %.2f\n", fahrenheit);
 
     return 0;
 }
