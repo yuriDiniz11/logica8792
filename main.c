af13f2991fa31e365244094a3df8a9c23902ee0e
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-14 21:09:02 -0300

    Aponta o menor número digitado

--- main.c
+++ main.c
@@ -7,19 +7,33 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int a, b, c, d, e, maior;
+   int a, b, c, d, e, menor;
 
-   printf("Digite cinco números: ");
-   scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+   printf("Digite um valor para a: ");
+   scanf("%d", &a);
 
-   maior = a;
+   printf("Digite um valor para b: ");
+   scanf("%d", &b);
 
-   if(b > maior) maior = b;
-   if(c > maior) maior = c;
-   if(d > maior) maior = d;
-   if(e > maior) maior = e;
+   printf("Digite um valor para c: ");
+   scanf("%d", &c);
 
-   printf("Maior: %d", maior);
+   printf("Digite um valor para d: ");
+   scanf("%d", &d);
+
+   printf("Digite um valor para e: ");
+   scanf("%d", &d);
+
+   menor = a;
+
+   if(b < menor) menor = b;
+   if(c < menor) menor = c;
+   if(d < menor) menor = d;
+   if(e < menor) menor = e;
+
+   printf("menor: %d", menor);
 
     return 0;
 }
+
+//testeeeee     
