98fe116b64e7ab10bbc63c9e745a702a90ffdf7c
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-14 21:26:46 -0300

    Mostra o maior e menor valor digitado

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int a, b, c, d, e, menor;
+   int a, b, c, d, e, menor, maior;
 
    printf("Digite um valor para a: ");
    scanf("%d", &a);
@@ -22,16 +22,23 @@ int main(){
    scanf("%d", &d);
 
    printf("Digite um valor para e: ");
-   scanf("%d", &d);
+   scanf("%d", &e);
 
    menor = a;
+   maior = a;
 
    if(b < menor) menor = b;
    if(c < menor) menor = c;
    if(d < menor) menor = d;
    if(e < menor) menor = e;
 
-   printf("menor: %d", menor);
+   if(b > maior) maior = b;
+   if(c > maior) maior = c;
+   if(d > maior) maior = d;
+   if(e > maior) maior = e;
+
+   printf("menor: %d\n", menor);
+   printf("maior: %d", maior);
 
     return 0;
 }
