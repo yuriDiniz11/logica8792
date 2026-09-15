62c87d77b20d1e69bf726b7de6bf3e8a52b48b1f
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-14 21:47:58 -0300

    Mostra se os números são iguais

--- main.c
+++ main.c
@@ -7,38 +7,16 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int a, b, c, d, e, menor, maior;
+   int a, b;
 
-   printf("Digite um valor para a: ");
-   scanf("%d", &a);
+   printf("Digite dois números: ");
+   scanf("%d %d", &a, &b);
 
-   printf("Digite um valor para b: ");
-   scanf("%d", &b);
-
-   printf("Digite um valor para c: ");
-   scanf("%d", &c);
-
-   printf("Digite um valor para d: ");
-   scanf("%d", &d);
-
-   printf("Digite um valor para e: ");
-   scanf("%d", &e);
-
-   menor = a;
-   maior = a;
-
-   if(b < menor) menor = b;
-   if(c < menor) menor = c;
-   if(d < menor) menor = d;
-   if(e < menor) menor = e;
-
-   if(b > maior) maior = b;
-   if(c > maior) maior = c;
-   if(d > maior) maior = d;
-   if(e > maior) maior = e;
-
-   printf("menor: %d\n", menor);
-   printf("maior: %d", maior);
+   if(a == b){
+    printf("Os números são iguais!\n");
+   }else{
+    printf("Os números são diferentes!\n");
+   }
 
     return 0;
 }
