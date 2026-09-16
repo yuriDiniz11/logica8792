97e8a2bb92d5b461657e25813a343842af3639ae
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-16 19:13:19 -0300

    Idade e altura para brinquedo

--- main.c
+++ main.c
@@ -7,16 +7,20 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int a, b;
+   int idade;
+   float altura;
 
-   printf("Digite dois números: ");
-   scanf("%d %d", &a, &b);
+   printf("Bem-vindos ao brinquedo roleta da morte!");
+   printf("\nQual sua idade?");
+   scanf("%d", &idade);
+   printf("\nQual sua altura?");
+   scanf("%f", &altura);
 
-   if(a == b){
-    printf("Os números são iguais!\n");
+   if((idade >= 18) && (altura >= 1.60)){
+    printf("Você pode brincar na roleta!");
    }else{
-    printf("Os números são diferentes!\n");
-   }
+    printf("Você não pode brincar na roleta!");
+    }
 
     return 0;
 }
