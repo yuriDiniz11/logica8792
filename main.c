217c0a9678c7634ca717616290822476518c5175
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-16 21:41:04 -0300

    Os lados de um triângulo (else if)

--- main.c
+++ main.c
@@ -7,26 +7,18 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   float n1, n2, n3, media;
+   float a, b, c;
 
-   printf("Digite a nota 1: ");
-   scanf("%f", &n1);
-   
-    printf("Digite a nota 2: ");
-    scanf("%f", &n2);
+   printf("Digite os três lados do triângulo: ");
+   scanf("%f %f %f", &a, &b, &c);
 
-    printf("Digite a nota 3: ");
-    scanf("%f", &n3);
-
-    media = (n1 + n2 + n3) /3;
-
-    if (media <= 4){
-        printf("Reprovado!");
-    }else if(media >= 7){
-        printf("Aprovado!!!");
-    }else{
-        printf("Recuperação!");
-    }
+   if(a == b && b == c){
+    printf("Equilátero!\n");
+   }else if(a == b || a == c || b == c){
+    printf("Isosceles!\n");
+   }else{
+    printf("Escaleno!\n");
+   }
 
 
 
