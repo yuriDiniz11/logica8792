5eabbd85a2085663150cac4ee796fb8b09d9a37f
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-18 19:15:54 -0300

    Sistema de pedágio 2.0

--- main.c
+++ main.c
@@ -12,15 +12,23 @@ int main(){
    printf("--- SISTEMA DE PEDÁGIO ---\n");
    printf("1 - Moto\n");
    printf("2 - Carro Passeio\n");
+   printf("3 - caminhão\n");
+   printf("4 - Ônibus\n");
    scanf("%d", &opcao);
 
    switch (opcao)
    {
     case 1:
         printf("Categoria: Moto | Tarifa: R$ 5,00\n");
-        break;
+        break; 
     case 2:
         printf("Categoria: Carro Passeio | Tarifa: R$ 10,00\n");
+        break;
+    case 3:
+        printf("Categoria: Caminhão | Tarifa: R$ 25,00\n");
+        break;
+    case 4:
+        printf("Categoria: Ônibus | Tarifa R$ 30,00\n");
         break;
     default:
         printf("Erro: Categoria não cadastrada no sistema.\n");
