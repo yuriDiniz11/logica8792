6fd719a2dcff165f5635d775bdbf577fd3f032c5
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-18 21:53:50 -0300

    Uso básico de FOR

--- main.c
+++ main.c
@@ -7,31 +7,9 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   float peso, altura, imc;
-
-   printf("Digite o peso (kg): ");
-   scanf("%f", &altura);
-
-   printf("Digite a altura: ");
-   scanf("%f", &peso);
-
-   imc = peso / (altura * altura);
-
-   if(imc < 18.5){
-        printf("Seu peso é: %f\n", peso);
-        printf("Sua altura é: %f\n", altura);
-        printf("Classificação: Abaixo do peso!\n");
-   }else if(imc < 25){
-        printf("Seu peso é: %f\n", peso);
-        printf("Sua altura é: %f\n", altura);
-        printf("Classificação: Peso normal!\n");
-   }else if(imc < 30){
-        printf("Seu peso é: %f\n", peso);
-        printf("Sua altura é: %f\n", altura);
-        printf("Classificação: Sobrepeso!\n");
-   }else{
-        printf("Classificação: Obesidade!\n");
+   for(int i = 1; i <= 100; i++){
+        printf("%d\n", i);
+        
    }
-
     return 0;
 }
