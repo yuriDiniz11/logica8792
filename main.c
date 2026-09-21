d5416df0e5bc1ddf0d065527325207236e44990b
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-21 19:49:47 -0300

    FOR: 10 números e 9 posições

--- main.c
+++ main.c
@@ -7,8 +7,11 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   for(int i = 0; i < 10000; i++){
-        printf("%d\n", i);
+   for(int i = 0; i < 10; i++){
+        printf("%s\n %d", "\nOs números estão aumentando: ---->>" ,i);
 }
+
+    printf("\nMeu for funcionou!!");
+
     return 0;
 }
