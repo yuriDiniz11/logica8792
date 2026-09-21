6e1bac4bc1ee59c75440dde8033991f96ead6b3e
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-21 20:07:54 -0300

    Convocados para seleção de futsal!!!

--- main.c
+++ main.c
@@ -7,11 +7,15 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   char frutas[3][20] = {"Maça", "Banana", "Laranja"};
+   char nomes[5][20] = {"Ronaldo", "Ronaldinho", "Ronaldão", "Ronaldowsky", "Ronald"};
 
-    printf("%s\n", frutas[0]);
-    printf("%s\n", frutas[1]);
-    printf("%s\n", frutas[2]);
+    printf("%s\n", nomes[0]);
+    printf("%s\n", nomes[1]);
+    printf("%s\n", nomes[2]);
+    printf("%s\n", nomes[3]);
+    printf("%s\n", nomes[4]);
+
+    printf("\nConvocados da seleção de futsal!!");
 
     return 0;
 }
