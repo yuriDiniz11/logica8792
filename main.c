08541090de3954c51edecf225fd67e407300836a
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-21 19:40:21 -0300

    FOR: imprimir até 9999 (posição)

--- main.c
+++ main.c
@@ -7,9 +7,8 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   for(int i = 1; i <= 100; i++){
+   for(int i = 0; i < 10000; i++){
         printf("%d\n", i);
-        
-   }
+}
     return 0;
 }
