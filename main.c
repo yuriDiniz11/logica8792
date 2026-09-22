18108c26cb3ac9cd5ece79dabc0fc20fd06f0fc3
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-22 20:09:18 -0300

    Calculadora (+ e -) usadno função

--- main.c
+++ main.c
@@ -1,18 +1,28 @@
 #include<stdio.h>
 #include<windows.h>
 
-void saudar(const char *nome){
-    printf("Olá %s\n", nome);
+int somar(int a, int b){
+    return a + b;
+}
+
+int subtrair(int a, int b){
+    return a - b;
 }
 
 int main(){
 
+    float a, b;
+
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   saudar("Yuri");
-   saudar("Gonçalves");
-   saudar("Diniz");
+   int resultado1 = somar(5, 3);
+   printf("O resultado da soma é: %d", resultado1);
+
+   int resultado2 = subtrair(5, 3);
+   printf("\nO resultado da subtração é: %d", resultado2);
+
+  
 
     return 0;
 }
