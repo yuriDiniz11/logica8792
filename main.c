57f589ca63314f86d127c7fe1d598c025e1e41cf
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-21 21:33:49 -0300

    Somando usando o FOR!

--- main.c
+++ main.c
@@ -7,9 +7,17 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   for(int i = 10; i >= 0; i = i - 2){
-    printf("%d\n", i);
+   int soma = 0;
+   int i;
+
+   for(i = 1; i <= 5; i++){
+    soma = soma + i;
    }
+
+   //Usando o printf fora das chaves do for ele cita número por número após soma!
+   //Usando o printf dentro das chaves ele da apenas o resultado final da soma!!
+
+   printf("Soma é %d\n", soma);
 
     return 0;
 }
