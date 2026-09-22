5b95aa399b5d07bebc281eabb97b7ac34d658872
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-22 19:25:05 -0300

    Ponteiro com void

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<windows.h>
 
+void dobrar(int *x){
+    *x = *x * 5;
+}
 
 int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int x = 10;
-   int *p = &x;
-
-   printf("Valor de x: %d\n", x);
-   printf("Endereço de x: %p\n", &x);
-   printf("Valor via ponteiro: %d\n", *p);
+   int numero = 10;
+   dobrar(&numero);
+   printf("Número depois da função %d\n", numero);
 
     return 0;
 }
