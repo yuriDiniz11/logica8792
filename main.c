c733ad71b7866b56200ae66a2146bbfdda59ea52
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-21 21:43:43 -0300

    FOR: declarou e sobrescreveu

--- main.c
+++ main.c
@@ -7,17 +7,11 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int soma = 0;
-   int i;
+   int numeros[] = {1, 2, 3};
 
-   for(i = 1; i <= 5; i++){
-    soma = soma + i;
-   }
+   numeros[0] = 10;
 
-   //Usando o printf fora das chaves do for ele cita número por número após soma!
-   //Usando o printf dentro das chaves ele da apenas o resultado final da soma!!
-
-   printf("Soma é %d\n", soma);
+   printf("%d", numeros[0]);
 
     return 0;
 }
