c19e08090daa1a0c8f87c00c4883d158668662a8
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-21 21:14:35 -0300

    FOR: decrescente de 10 a 0 diminuindo 2

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   for(int i = 0; i <= 10; i = i + 2){
+   for(int i = 10; i >= 0; i = i - 2){
     printf("%d\n", i);
    }
 
