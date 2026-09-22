8ab2ff1fc5e5684ec5fe3a9c61911eeca5fdd29f
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-21 21:03:48 -0300

    Array mostrando idades %s, %d, %s

--- main.c
+++ main.c
@@ -7,29 +7,11 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int numerosInteiros[5] = {1, 5, 10, 15, 20};
-   float numerosFloats[5] = {10.5, 20.8, 30.2, 40.9, 50.1};
-   char carros[3][20] = {"Ferrari", "Mercedez", "Mazda"};
+   int idades[3] = {25, 35, 45};
 
-   printf("Mostraremos os números inteiros primeiro:\n");
-
-   for(int i = 0; i < 5; i++){
-    printf("%d\n", numerosInteiros[i]);
-   }
-
-   printf("\nAgora mostraremos os números floats:\n");
-
-   for(int j = 0; j < 5; j++){
-    printf("%.2f\n", numerosFloats[j]);
-   }
-
-   printf("\nAgora mostraremos os veículos:\n");
-
-   for(int k = 0; k < 3; k++){
-    printf("%s\n", carros[k]);
-   }
-
-   
+   printf("%s %d %s", "Manoel tem,", idades[0], "anos!");
+   printf("%s %d %s", "\nRafael tem,", idades[1], "anos!!");
+   printf("%s %d %s", "\nRodrigo tem,", idades[2], "anos!!!");   
 
     return 0;
 }
