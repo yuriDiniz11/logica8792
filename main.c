4a1c2887c8dda723626de2db706d6303ec10a560
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-23 20:57:08 -0300

    Verificação de impar\par

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 #include<windows.h>
 #include<math.h>
 
-int tabuada(int n){
-    printf("Tabuada do %d:\n", n);
-    for(int i = 1; i <= 10; i++){
-        printf("%d x %d = %d\n", n, i, n * i);
+void verificarEhParImpar(int n){
+    if(n % 2 == 0){
+        printf("%d é par!\n");
+    }else{
+        printf("%d é impar!\n", n);
     }
-    return 0;
 }
 
 int main(){
@@ -15,10 +15,8 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int numero;
-    printf("Digite um número para tabuada: ");
-    scanf("%d", &numero);
-    tabuada(numero);
+    verificarEhParImpar(10);
+    verificarEhParImpar(7);
 
     return 0;
 }
