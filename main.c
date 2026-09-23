69d11f66d1117ed104f460508efd5ec91ed8176f
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-22 21:40:40 -0300

    Citando nomes com FOR (DNV)

--- main.c
+++ main.c
@@ -6,23 +6,14 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int horas[12];
-    int minutos[60];
-
-    for(int i = 0; i < 12; i++){
-        horas[i] = i + 1;
-    }
-  
-    for(int j = 0; j < 60; j++){
-        minutos[j] = j;
-    }
-
-    //simula o relógio
-    for(int i = 0; i < 12; i++){
-        for(int j = 0; j < 60; j++){
-            printf("Hora: %2d:%02d\n", horas[i], minutos[j]);
+    char nomes[3][20] ={
+        "Ronaldo",
+        "Ronaldinho",
+        "Ronaldão"
+    };
+        for(char i = 0; i < 3; i++){
+            printf("%s\n", nomes[i]);
         }
-    }
 
     return 0;
 }
