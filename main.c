b236f1c9fc9c193a9e2eff806f01e90b4fd8d74d
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-22 21:52:54 -0300

    FGETS: lendo o nome e imprimindo

--- main.c
+++ main.c
@@ -7,14 +7,12 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int numero;
-    printf("Digite um número: ");
-    scanf("%d", &numero);
+    char nome[50];
 
-    int quadrado = (int)pow(numero, 2);
-    int cubo = (int)pow(numero, 3);
-    printf("Quadrado de %d = %d\n", numero, quadrado);
-    printf("Cubo de %d = %d", numero, cubo);
+    printf("Digite seu nome completo: ");
+    fgets(nome, sizeof(nome), stdin); //lê até encontrar ENTER
+
+    printf("Olá %s", nome);
 
     return 0;
 }
