ea8bccdd58ad35277781f249368cd55528d56517
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-23 19:52:03 -0300

    Fibonacci Função

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include<windows.h>
 #include<math.h>
 
-int fatorial(int n){
-    int resultado = 1;
-    for(int i = 1; i <= n; i++){
-        resultado *= i; //*= significa: resultado = resultado * i;
+void fibonacci(int termos){
+    int a = 0, b = 1, c;
+    printf("Sequência de Fibonacci (%d termos): ", termos);
+    for(int i = 1; i <= termos; i++){
+        printf("%d", a);
+        c = a + b;
+        a = b;
+        b = c;
     }
-    return resultado;
+    printf("\n");
 }
 
 int main(){
@@ -15,8 +19,7 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int numero = 5;
-    printf("Fatorial de %d = %d\n", numero, fatorial(numero));
+    fibonacci(10);
 
     return 0;
 }
