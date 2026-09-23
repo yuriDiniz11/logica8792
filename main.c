d947c925b56ae2f5fa5cf44180d294b5b3dfea40
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-22 21:26:25 -0300

    Relógio usando FOR

--- main.c
+++ main.c
@@ -6,13 +6,23 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int num[5] = {1, 2, 3, 4, 5};
+    int horas[12];
+    int minutos[60];
 
-    for(int i = 0; i < 5; i++){
-       printf("%d\n", num[i]);
+    for(int i = 0; i < 12; i++){
+        horas[i] = i + 1;
     }
   
-        
+    for(int j = 0; j < 60; j++){
+        minutos[j] = j;
+    }
+
+    //simula o relógio
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 60; j++){
+            printf("Hora: %2d:%02d\n", horas[i], minutos[j]);
+        }
+    }
 
     return 0;
 }
