509447aa6081baa276fcfd1825da13db3780ae52
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-24 20:10:37 -0300

    While e switch: Caixa

--- main.c
+++ main.c
@@ -8,16 +8,25 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int numero;
+    int opcao = -1;
 
-    printf("Digite um n° positivo: ");
-    scanf("%d", &numero);
+    while(opcao != 0){
+        printf("\nMenu: \n");
+        printf("1 - Depositar\n");
+        printf("2 - Sacar\n");
+        printf("3 - Consultar\n");
+        printf("0 - sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
 
-    while(numero <= 0){
-        printf("N° inválido! Digite novamente: ");
-        scanf("%d", &numero);
+        switch(opcao){
+            case 1: printf("Você escolheu Depositar\n"); break;
+            case 2: printf("Você escolheu Sacar\n"); break;
+            case 3: printf("Você escolheu Consultar\n"); break;
+            case 0: printf("Saindo ..."); break;
+            default: printf("Opção inválida!\n"); break;
+        }
     }
-    printf("N° válido: %d\n", numero);
 
     return 0;
 }
