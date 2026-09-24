eafbd8501955044be76d3d3606c42f0e6b8c36dc
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-24 20:20:26 -0300

    While soma

--- main.c
+++ main.c
@@ -8,25 +8,16 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int opcao = -1;
+    int numero, soma = 0;
 
-    while(opcao != 0){
-        printf("\nMenu: \n");
-        printf("1 - Depositar\n");
-        printf("2 - Sacar\n");
-        printf("3 - Consultar\n");
-        printf("0 - sair\n");
-        printf("Escolha: ");
-        scanf("%d", &opcao);
+    printf("Digite número para somar (0 encerra): ");
+    scanf("%d", &numero);
 
-        switch(opcao){
-            case 1: printf("Você escolheu Depositar\n"); break;
-            case 2: printf("Você escolheu Sacar\n"); break;
-            case 3: printf("Você escolheu Consultar\n"); break;
-            case 0: printf("Saindo ..."); break;
-            default: printf("Opção inválida!\n"); break;
-        }
+    while(numero != 0){
+        soma += numero;
+        scanf("%d", &numero);
     }
+    printf("Soma total = %d\n", soma);
 
     return 0;
 }
