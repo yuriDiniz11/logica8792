a21d1eb6df411b9363dffc94bcefc395f6b92122
yuriDiniz11 <dinizyuri88@gmail.com>
2026-09-24 19:10:16 -0300

    While 1° vista

--- main.c
+++ main.c
@@ -2,22 +2,19 @@
 #include<windows.h>
 #include<math.h>
 
-void maxMin(int v[], int tamanho){
-    int max = v[0], min = v[0];
-    for(int i = 1; i < tamanho; i++){
-        if(v[i] > max) max = v[i];
-        if(v[i] < min) min = v[i];
-    }
-    printf("Máximo: %d, Mínimo: %d\n", max, min);
-}
 
 int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int numeros[] = {4, 8, 2, 15, 6};
-    maxMin(numeros, 5);
+    int i = 0;
+
+    while(i <= 5){
+        printf("%d\n", i);
+        i++;
+    }
+    
 
     
 
